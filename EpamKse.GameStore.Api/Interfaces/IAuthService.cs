8a8e3033faa897f00b4af3ff05f681da374ce748
DavidBakalov21 <davidbakalov33@gmail.com>
2025-05-23 16:11:37 +0300

    refresh seems to work, auto test

--- EpamKse.GameStore.Api/Interfaces/IAuthService.cs
+++ EpamKse.GameStore.Api/Interfaces/IAuthService.cs
@@ -4,7 +4,8 @@ namespace EpamKse.GameStore.Api.Interfaces;
 
 public interface IAuthService
 {
-    public Task<string> Register(RegisterDTO registerDto);
+    public Task<(string, string)> Register(RegisterDTO registerDto);
     
-    public Task<string> Login(LoginDTO loginDto);
+    public Task<(string, string)> Login(LoginDTO loginDto);
+    public Task<(string, string)> Refresh(string email);
 }
