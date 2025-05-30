87da9d1a055241c588b00433d638e5587e3de695
DavidBakalov21 <davidbakalov33@gmail.com>
2025-05-30 11:45:19 +0300

    Created db entities

---
+++ EpamKse.GameStore.DataAccess/Configurations/PublisherConfiguration.cs
@@ -0,0 +1,37 @@
+using EpamKse.GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EpamKse.GameStore.DataAccess.Configurations;
+
+public class PublisherConfiguration: IEntityTypeConfiguration<Publisher>
+{
+    public void Configure(EntityTypeBuilder<Publisher> builder)
+    {
+        builder.ToTable("Publishers");
+        
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+            .HasColumnName("Id")
+            .ValueGeneratedOnAdd();
+        
+        builder.Property(e => e.Name)
+            .HasColumnName("Name")
+            .IsRequired();
+        
+        builder.Property(c => c.CreatedAt)
+            .HasDefaultValueSql("GETDATE()")
+            .IsRequired();
+        builder
+            .HasMany(p => p.PublisherPlatforms)
+            .WithMany(p => p.Publishers)
+            .UsingEntity(j => j.ToTable("PublisherPlatforms"));
+        
+        builder.HasMany(p => p.Games)
+            .WithOne(g => g.Publisher)
+            .HasForeignKey(g => g.PublisherId)
+            .OnDelete(DeleteBehavior.Cascade);
+            
+    }
+}
