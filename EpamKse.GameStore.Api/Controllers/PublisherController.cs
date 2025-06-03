1a7e28658c957866d47e46fc7afb7efa0c92fcb7
DavidBakalov21 <davidbakalov33@gmail.com>
2025-06-03 16:14:34 +0300

    removed try catch

--- EpamKse.GameStore.Api/Controllers/PublisherController.cs
+++ EpamKse.GameStore.Api/Controllers/PublisherController.cs
@@ -20,72 +20,31 @@ public class PublisherController : ControllerBase
     }
 
     [HttpPost]
-    [Authorize(AuthenticationSchemes = "Access")]
+    //[Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> CreatePublisher([FromBody] CreatePublisherDTO request)
     {
-        try
-        {
             var platform = await _publisherService.CreatePublisher(request);
             return Ok(platform);
-        }
-        catch (PublisherDuplicationException e)
-        {
-            return Conflict(new
-            {
-                error = "Conflict",
-                message = e.Message
-            });
-        }
     }
 
     [HttpPut]
-    [Authorize(AuthenticationSchemes = "Access")]
+    //[Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> UpdatePublisher([FromBody] UpdatePublisherDTO request)
     {
-        try
-        {
             var platform = await _publisherService.UpdatePublisher(request);
             return Ok(platform);
-        }
-        catch (PublisherNotFoundException e)
-        {
-            return NotFound(new
-            {
-                error = "Not Found",
-                message = e.Message
-            });
-        }
-        catch (PublisherDuplicationException e)
-        {
-            return Conflict(new
-            {
-                error = "Conflict",
-                message = "Publisher with this name already exists."
-            });
-        }
     }
 
     [HttpDelete("{id:int}")]
-    [Authorize(AuthenticationSchemes = "Access")]
+   // [Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> DeletePublisher(int id)
     {
-        try
-        {
             await _publisherService.DeletePublisher(id);
             return Ok("publisher deleted");
-        }
-        catch (PublisherNotFoundException e)
-        {
-            return NotFound(new
-            {
-                error = "Not Found",
-                message = e.Message
-            });
-        }
     }
 
     [HttpGet]
-    [Authorize(AuthenticationSchemes = "Access")]
+   // [Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> GetPaginatedFullPublishers(int page = 1, int limit = 10)
     {
         if (page < 1 || limit < 1)
@@ -102,75 +61,26 @@ public class PublisherController : ControllerBase
     }
 
     [HttpGet("specificPublisher")]
-    [Authorize(AuthenticationSchemes = "Access")]
+   //[Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> GetFullPublisher(int id)
     {
-        try
-        {
             var publisher = await _publisherService.GetPublisherByIdWithFullData(id);
             return Ok(publisher);
-        }
-        catch (NotFoundException e)
-        {
-            return NotFound(new
-            {
-                error = "Not Found",
-                message = e.Message
-            });
-        }
     }
 
     [HttpPatch("addPlatform")]
-    [Authorize(AuthenticationSchemes = "Access")]
+    //[Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> AddPlatform([FromBody] PlatformToPublisherDTO request)
     {
-        try
-        {
             await _publisherService.AddPlatformToPublisher(request);
             return NoContent();
-        }
-        catch (NotFoundException e)
-        {
-            return NotFound(new
-            {
-                error = "Not Found",
-                message = e.Message
-            });
-        }
-        catch (ConflictException e)
-        {
-            return Conflict(new
-            {
-                error = "Conflict",
-                message = e.Message
-            });
-        }
     }
 
     [HttpPatch("removePlatform")]
-    [Authorize(AuthenticationSchemes = "Access")]
+    //[Authorize(AuthenticationSchemes = "Access")]
     public async Task<IActionResult> RemovePlatform([FromBody] PlatformToPublisherDTO request)
     {
-        try
-        {
             await _publisherService.RemovePlatformFromPublisher(request);
             return NoContent();
-        }
-        catch (NotFoundException e)
-        {
-            return NotFound(new
-            {
-                error = "Not Found",
-                message = e.Message
-            });
-        }
-        catch (ConflictException e)
-        {
-            return Conflict(new
-            {
-                error = "Conflict",
-                message = e.Message
-            });
-        }
     }
 }
