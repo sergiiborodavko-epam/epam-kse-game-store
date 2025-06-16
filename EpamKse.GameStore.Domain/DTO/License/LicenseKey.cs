245093274d31d8af7341d7dcbf076f9c1aa652fb
yaroslavdrapei <yarikdrapey@gmail.com>
2025-06-16 19:03:49 +0300

    feat: license tests and minor fixed

--- EpamKse.GameStore.Domain/DTO/License/LicenseKey.cs
+++ EpamKse.GameStore.Domain/DTO/License/LicenseKey.cs
@@ -3,6 +3,6 @@ namespace EpamKse.GameStore.Domain.Entities;
 public class LicenseKey
 {
     public int OrderId { get; set; }
-    public IEnumerable<int> GameIds = [];
+    public IEnumerable<int> GameIds { get; set; } = [];
     public int UserId { get; set; }
 }
